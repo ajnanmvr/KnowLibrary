358477626faf0a23d0284db5a035cef307559b25
ajnanmvr <ajnanpulikkathody@gmail.com>
2023-06-15 11:49:26 +0530

    axios change

--- src/axios.js
+++ src/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const Axios = axios.create({
-baseURL: "http://localhost:3100/",
-// baseURL: "https://knowlib-backend.vercel.app/",
+// baseURL: "http://localhost:3100/",
+baseURL: "https://knowlib-backend.vercel.app/",
   headers: {
     "Content-Type": "application/json",
     "Access-Control-Allow-Origin": "*",
